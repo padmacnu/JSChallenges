6c3457af7d08b418a463a7e16eba017953e3d1ad
Srinivas Padma <padmacnu@users.noreply.github.com>
2017-05-20 20:45:01 -0700

    Create findOddNumbers.js
    Function that would return an array of all the odd numbers between 1 to 50

---
+++ findOddNumbers.js
@@ -0,0 +1,11 @@
+// Write a function that would return an array of all the odd numbers between 1 to 50. (ex. [1,3,5, .... , 47,49])
+
+function oddNumbers() {
+    var arr = [];
+    for(var i = 1; i <= 50; i++ ) {
+        if(i % 2 === 1) {
+            arr.push(i);
+        }
+    }
+    return arr; 
+}
