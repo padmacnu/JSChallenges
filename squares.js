480a4f79e503303fced2bf198d860e61e52e1837
Srinivas Padma <padmacnu@users.noreply.github.com>
2017-05-21 01:29:59 -0700

    Create squares.js
    Function that replaces each value in the array with the product of the original value squared by itself

---
+++ squares.js
@@ -0,0 +1,14 @@
+/*
+  Squares
+  Given an array with multiple values, write a function that replaces each value in the array with the product of the original value squared by itself. 
+  (e.g. [1,5,10,-2] will become [1,25,100,4])
+*/
+
+function sqauresVal() {
+  for(var i = 0; i < arr.length; i++) {
+    arr[i] = arr[i] * arr[i];
+  }
+  return arr;
+}
+
+squaresVal([2,3,4,5]);
