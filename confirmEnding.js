236ac0b20dad4b45b7c96710b484503e7e78e226
Srinivas Padma <padmacnu@users.noreply.github.com>
2017-05-31 23:13:27 -0700

    Update confirmEnding.js

--- confirmEnding.js
+++ confirmEnding.js
@@ -9,7 +9,7 @@ function confirmEnding(string, target) {
 confirmEnding("Srinivas", "v");
 
 
-// Approach #2: Using a ternary operator as a shortcut for the if statement:
+// Using a ternary operator as a shortcut for the if statement:
 function confirmEnding(string, target) {
  return (string.substr(-target.length) === target) ? true : false;
 }
@@ -17,14 +17,12 @@ confirmEnding("Srinivas", "s");
 
 
 /*
-  Approach #3: Confirm the Ending of a String With Built-In Functions — with endsWith()
+  Approach #2: Confirm the Ending of a String With Built-In Functions — with endsWith()
   The endsWith() method determines whether a string ends with the characters of 
   another string, returning true or false as appropriate. 
   This method is case-sensitive.
 */
 function confirmEnding(string, target) {
-  // We return the method with the target as a parameter
-  // The result will be a boolean (true/false)
   return string.endsWith(target); // 'Bastian'.endsWith('n')
 }
 confirmEnding('Bastian', 'n');
