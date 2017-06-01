fae923e121301ef9e872a094e5e996baa5968d19
Srinivas Padma <padmacnu@users.noreply.github.com>
2017-06-01 11:58:32 -0700

    Update ReverseStringWithLoop.js

--- ReverseStringWithLoop.js
+++ ReverseStringWithLoop.js
@@ -2,7 +2,6 @@
 
 function reverseString(str) {
   let newString = "";
-  debugger;
 
   for (let i = str.length - 1; i >= 0; i--) {
     newString += str[i]; // // or newString = newString + str[i];
