0ea6d6039243223e8e3f58e111063408a75b37ed
Srinivas Padma <padmacnu@users.noreply.github.com>
2017-09-27 18:15:10 -0700

    Update Reversingstring.js

--- Reversingstring.js
+++ Reversingstring.js
@@ -7,7 +7,7 @@ reverseString("Srinivas");
 function reverseString(str) {
 debugger;
 	var splitString = str.split(""); 
-   // splitString = (8) ["s", "a", "v", "i", "n", "i", "r", "S"]
+   // splitString = (8) ["S", "r", "i", "n", "i", "v", "a", "s"], str = "Srinivas"
 	
   var reverseArray = splitString.reverse();
    // reverseArray = (8) ["s", "a", "v", "i", "n", "i", "r", "S"]
